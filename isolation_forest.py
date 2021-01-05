85aae5cbbfcde7d249f1478c48ff76413336bf61
kevin-tete <alanjohnsatan@gmail.com>
2021-01-05 12:30:34 +0100

    second commit

--- isolation_forest.py
+++ isolation_forest.py
@@ -17,23 +17,22 @@ import re
  
 
 df = pd.read_csv("/Users/kevin-tete/Downloads/temperature.csv")
+
 df['temperature'] = df['temperature;EnqueuedTime;ConnectionDeviceId'].map(lambda x :re.sub('[,\!? ;]', '', x[0:5]))
 df['date'] = df['temperature;EnqueuedTime;ConnectionDeviceId'].map(lambda x :re.sub('[,\!? ;]', '', x[6:16]))
-df['time'] = df['temperature;EnqueuedTime;ConnectionDeviceId'].map(lambda x :re.sub('[,\!? ;]', '', x[17:19]))
+df['time'] = df['temperature;EnqueuedTime;ConnectionDeviceId'].map(lambda x :re.sub('[,\!? ;]', '', x[17:25]))
 df['conectiondeviced'] = df['temperature;EnqueuedTime;ConnectionDeviceId'].map(lambda x :re.sub('[,\!? ;]', '', x[30:38]))
 df = df.drop('temperature;EnqueuedTime;ConnectionDeviceId', axis = 1)
-print(df[['temperature','conectiondeviced']])
+
 
 encoder = OneHotEncoder(sparse=False)
 
 df['conectiondeviced']=encoder.fit_transform(df[['conectiondeviced']])
+df['time']=pd.to_datetime(df['time'],format='%H:%M:%S')
+df['Hour'] = df['time'].dt.hour 
+df['minute'] = df['time'].dt.minute 
 
-# # Organisation du dataset en fonction des heures d'entrée 
 counts = df.groupby('time')
-#size = counts.size() # ici , on compte le nombre de données recues (temperatures ) par heure 
-# # dans ce boxplot , on peut se rendre compte qu'il ya certaines fois ou les données sont recues plus d'une fois. 
-# #Nous allons étudier un peu plus dans le details si ces données sont des anomalies.
-#size.plot(kind = 'bar') 
 
 y = df['temperature']
 
@@ -70,43 +69,29 @@ plt.hist(y, bins, range, color = 'green',
 plt.xlabel("temperature en degrés celsius")
 plt.ylabel("occurences")
 plt.savefig('fig2.png', dpi = 400)
+
 plt.show()
-#plt.plot(y,time , 'ro', label = 'actual')
-#plt.xticks(rotation = '60'); 
-#plt.legend()
-# #Organisation du dataset en fonction des dates 
-# counts1 = df.groupby('date')
-# size = counts1.size()
-# size.plot(kind = 'bar')
-#on veut voir une anomalie sur les temperatures ???
-#on va donc isoler le dataset sur les temperatures
-#on va utiliser l'algorithme de random forest
-#on calcule la moyenne et l'ecart type des temperatures afin  de determiner une fonction
-#densité de probabilité et de se servir de cette fonction pour calculer 
-#la probabilité d'existence d'un echantillon
-#dans notre cas on essaie de voir si dans le dataset des temperatures , nous avons des temperatures extreme ou minimum par jour , heure 
-#X = df.drop(['date'], axis = 1)
+
 model1 = IsolationForest(contamination=0.01)
-print(model1.fit(df[['temperature','conectiondeviced']]))
+print(model1.fit(df[['temperature','conectiondeviced','Hour','minute']]))
 
 
-df['scores']=model1.decision_function(df[['temperature','conectiondeviced']])
+df['scores']=model1.decision_function(df[['temperature','conectiondeviced','Hour','minute']])
 
-df['anomaly']=model1.predict(df[['temperature','conectiondeviced']])
+df['anomaly']=model1.predict(df[['temperature','conectiondeviced','Hour','minute']])
 print (df['anomaly'])
 print(df.head(50))
-
 pyplot.plot(df['scores'], df['temperature'],linestyle = 'none', marker = 'o', c = 'lime',
   markersize = 5)
 pyplot.xlabel("scores")
 pyplot.ylabel("temperatures")
 pyplot.savefig('fig1.png', dpi = 400)
+
+
+plt.legend()
+plt.show();
 pyplot.show()
 print("-----------------")
 print(df['anomaly'].value_counts())
 
-"""
-from sklearn.datasets import make_blobs
-x,y = make_blobs(n_samples=100)
-print(x)
-"""
+
